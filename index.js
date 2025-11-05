3e8c113309f2921ebbf25f2a787fe1a285eace54
justkartiktyagi <kartik20tyagi05@gmail.com>
2025-11-05 21:36:22 +0530

    prepare backend for render deployment

--- index.js
+++ index.js
@@ -2,25 +2,29 @@ const express = require("express");
 const path = require("path");
 const cors = require("cors");
 const mongoose = require("mongoose");
+require("dotenv").config();
+
 const app = express();
 const { storeRouter } = require("./Routes/storeRouter");
-const MONGO_URL =
-  "mongodb+srv://expensetracker:expensetracker@cluster0.53qjbx5.mongodb.net/expenses?retryWrites=true&w=majority&appName=Cluster0";
-// ✅ This parses JSON bodies (API requests)
-app.use(cors()); // ✅ allow all origins
-app.use(express.json());
 
-// ✅ This parses HTML <form> submissions
+// ✅ Middlewares
+app.use(cors());
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
   console.log(`${req.method} ${req.url}`);
   next();
 });
+
+// ✅ Routes
 app.use("/expense", storeRouter);
 
-const PORT = 3000;
+// ✅ Port setup for Render
+const PORT = process.env.PORT || 3000;
+
+// ✅ MongoDB connection
 mongoose
-  .connect(MONGO_URL)
+  .connect(process.env.MONGO_URL)
   .then(() => {
     console.log("✅ MongoDB Connected Successfully");
     app.listen(PORT, () => {
